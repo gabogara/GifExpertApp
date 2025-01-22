700236302a5ed76548d13b90d797129b20a1520c
gabogara <gabo1gara@gmail.com>
2025-01-21 23:22:05 -0800

    improve addCategory component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,12 +4,12 @@ import { AddCategory } from './components/addCategory';
 
 export const GifExpertApp = () => {
 
-const [categories, setCategories] = useState( ['One Punch', 'Dragon Ball']);
+const [ categories, setCategories ] = useState([ 'One Punch' ]);
 
 
-const onAddCategory = () => {
-    setCategories([ 'NewCategory', ...categories ]);
-   // setCategories( cat => [ 'NeyCategory', ...categories ]);
+const onAddCategory = ( newCategory ) => {
+    setCategories([ newCategory, ...categories ]);
+   // setCategories( cat => [ '...cat', 'Valorant' ]);
 }
 
   return (
@@ -18,11 +18,12 @@ const onAddCategory = () => {
         <h1> GifExpertApp </h1>
     
     {/* Input  */}
-    <AddCategory/>
+    <AddCategory 
+                onNewCategory={ (value) => onAddCategory(value) }
+    />
 
 
     {/* Listado de Gif */}
-    <button onClick={ onAddCategory }> Agregar </button>
         <ol>
             {
                 categories.map( category => {
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 
-export const AddCategory = () => {
-    const [inputValue, setInputValue] = useState('One Punch');
+export const AddCategory = ({ onNewCategory }) => {
+    const [inputValue, setInputValue] = useState();
 
     const onInputChanged = ({target}) => {
-        console.log(target.value);
         setInputValue(target.value);
     };
 
     const onSubmit = (event) => {
         event.preventDefault();
         console.log(inputValue);
+        
+        if ( inputValue.trim().length <= 1) return;
+
+        //setCategories ( categories => [inputValue, ...categories ] );
+        onNewCategory ( inputValue.trim() );
+        setInputValue ('');
+        
     }
 
     return (
