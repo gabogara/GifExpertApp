76c443f07242868b1b137dc801f3ba37a8abd823
gabogara <gabo1gara@gmail.com>
2025-01-22 23:36:21 -0800

    add useEffect to gifGrid

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,36 @@
 import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+
 
 export const GifGrid = ({category}) => {
 
-    const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState( [] );
+
+    const getImages = async() => {
+        const newImages = await getGifs( category );
+        setImages(newImages);
+    }
 
     useEffect( () => {
-        getGifs(category); 
-    }, []);
-
-    getGifs(category);
+        getImages();
+    }, [])
+                     
 
     return (
             <>
-                        <h3> {category} </h3>
-                        <h5> {counter} </h5>
-                        <button onClick={ () => setCounter (counter + 1) }> +1 </button>
+                    <h3> {category} </h3>
+                    <div className="card-grid">
+                {
+                    images.map( ( image ) => (
+                        <GifItem 
+                            key={ image.id } 
+                            { ...image }
+                        />
+                    ))
+                }
+                
+            </div>
             </>
         )
 }
