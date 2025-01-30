695bb2c272feedf7ab7418f33d8656a6928543fd
gabogara <gabo1gara@gmail.com>
2025-01-29 19:43:06 -0800

    add useFewchGifs hook to load the images

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,15 @@
 import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
 
 
 export const GifGrid = ({category}) => {
+                    
 
-    const [images, setImages] = useState( [] );
-
-    const getImages = async() => {
-        const newImages = await getGifs( category );
-        setImages(newImages);
-    }
-
-    useEffect( () => {
-        getImages();
-    }, [])
-                     
+    const { images, isLoading } = useFetchGifs ( category);        
+    console.log( { images, isLoading} ); 
 
     return (
             <>
